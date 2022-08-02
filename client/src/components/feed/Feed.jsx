d34b0212d3cba20518007a1ad0c9d75b4dcd4eb1
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-08-02 16:52:07 +0600

    feed

--- client/src/components/feed/Feed.jsx
+++ client/src/components/feed/Feed.jsx
@@ -1,10 +1,13 @@
 import React from 'react'
 import './feed.css'
-
+import Share from '../share/Share'
 export default function Feed() {
   return (
     <div className='feed'>
-        Feed
+        <div className='feedWrapper'>
+        <Share/>
+        </div>
+        
     </div>
   )
 }
