8c5bed5c99228739b837b9f60ea3bf0188ae9947
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-08-04 16:46:56 +0600

    rightbar

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -3,7 +3,243 @@ import './rightbar.css'
 export default function Rightbar() {
   return (
     <div className='rightbar'>
-        Rightbar
+        <div className='rightbarWrapper'>
+            <div className='birthdayContainer'>
+              <img src='assets/gift.png' alt=''className='birthdayImg'/>
+              <span className='birthdayText'>
+                <b>Ahmed Xony</b> and <b>3 other friends</b> have birthday today.
+              </span>
+            </div>
+            <img src='assets/earn.webp' alt='' className='rightbarAd'/>
+            <h4 className='rightbarText'>Online Friends</h4>
+            <ul className='rightbarFriendList'>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+              <li className='rightbarFriend'>
+                <div className='rightbarProfileImgContainer'>
+                  <img src='assets/profile/3.jpg' alt='' className='rightbarProfileImg'/>
+                  <span className='rightbarOnline'></span>
+                </div>
+                <sapn className='rightbarUsername'>Ahmed Xony</sapn>
+              </li>
+            </ul>
+        </div>
     </div>
   )
 }
---
+++ client/src/dummyData.js
@@ -0,0 +1,39 @@
+export const users = [
+ {
+    id:1,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:2,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:3,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:4,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:5,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:6,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ },
+ {
+    id:7,
+    profilePicture:"assets/profile/1.jpg",
+    username:'Akash Ahmed'
+ }
+
+]
+
