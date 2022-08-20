62d34093a2017af30c0cfa7d2275e29822f342ef
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-08-20 17:18:28 +0600

    profile

--- client/src/components/pages/profile/Profile.jsx
+++ client/src/components/pages/profile/Profile.jsx
@@ -13,7 +13,14 @@ export default function Profile() {
             <Sidebar/>
               <div className='profileRight'>
                 <div className='profileRightTop'>
-                     hey   
+                     <div className='profileCover'>
+                        <img src='assets/profile/akashpost.jpg'className='profileCoverImg' alt=''/>
+                        <img src='assets/profile/rizvi.jpg'className='profileUserImg' alt=''/>
+                     </div>
+                     <div className='profileInfo'>
+                        <h4 className='profileInfoName'>Rizvi Ahmed</h4>
+                        <span className='profileInfoDesc'>hi this is my profile</span>
+                     </div>
                 </div>
                 <div className='profileRightBottom'>
                   <Feed/>
