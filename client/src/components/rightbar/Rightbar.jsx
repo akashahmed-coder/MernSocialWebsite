438f56ab299d229e7b8d7c5070a552ccb9c29b3b
akashahmed-coder <86545072+akashahmed-coder@users.noreply.github.com>
2022-08-21 17:56:56 +0600

    profile rightbar

--- client/src/components/rightbar/Rightbar.jsx
+++ client/src/components/rightbar/Rightbar.jsx
@@ -30,7 +30,50 @@ const HomeRigthBar = () => {
 const ProfileRightBar = () => {
   return(
     <>
-       hi
+       <h4 className='rightbarTitle'>User information</h4>
+       <div className='rightbarInfo'>
+           <div className='rightbarInfoItem'>
+             <span className='rightbarInfoKey'>City: </span>
+             <span className='rightbarInfoValue'>Dhaka </span>
+           </div>
+           <div className='rightbarInfoItem'>
+             <span className='rightbarInfoKey'>Country: </span>
+             <span className='rightbarInfoValue'>Bangladesh </span>
+           </div>
+           <div className='rightbarInfoItem'>
+             <span className='rightbarInfoKey'>Relationship: </span>
+             <span className='rightbarInfoValue'>Single </span>
+           </div>
+       </div>
+       <h4 className='rightbarTitle'>User Friends</h4>
+       <div className='rightbarFollowings'>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/sobuj.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/xony.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/rizvi.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/sahalom.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/rasu.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+        <div className='rightbarFollowing'>
+            <img className='rightbarFollowingImg' src='assets/profile/tawsif.jpg' alt=''/>
+            <span className='rightBarFollowingName'>Ahmed Sobuj</span>
+        </div>
+
+       </div>
+
     </>
   )
 }
